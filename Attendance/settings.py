a32868b15c778d5eab049f93b1b062b69a654425
omegapoint0001 <omegapoint0001@gmail.com>
2025-02-18 15:27:49 +0530

    Configured MySQL with Django

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'Attendance.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendance_db',  # Your MySQL database name
+        'USER': 'django_user',    # Your MySQL username
+        'PASSWORD': 'password',   # Your MySQL password
+        'HOST': 'localhost',      # Or the host of your MySQL server
+        'PORT': '3306',           # Default MySQL port
     }
 }
 
